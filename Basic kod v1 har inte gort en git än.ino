a6346d713e97e76075f89fc7f20aafd84a33d9be
danjo <Danjonalg@gmail.com>
2026-03-26 10:25:13 +0100

    test comit 2

--- Basic kod v1 har inte gort en git än.ino
+++ Basic kod v1 har inte gort en git än.ino
@@ -16,7 +16,7 @@ int alt_4=7;
 int qna =0;
 int anser =0;
 
-// heloooeheleoeoehekeo
+// heloooeheleoeoehekeo//this is a usles comment and sould be removed
 
 unsigned long startTime= millis(); // store start time;
 
